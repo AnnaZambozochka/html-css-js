13c892e5e199bfe96fac481acf79a5df52b208aa
AnnaZambozochka <80975250+AnnaZambozochka@users.noreply.github.com>
2025-09-13 01:26:10 +0300

    Update script.js
    ничего не работает

--- html-css/js/script.js
+++ html-css/js/script.js
@@ -1,1 +1,15 @@
-const a = document.querySelector('a');
+const inp1 = document.querySelector('inp1');
+const inp2 = document.querySelector('inp2');
+const resultBtn = document.querySelector('result');
+
+resultBtn.onclick = function ipp12(result) {
+     if (inp1.value > 0){
+        const result1 = inp1.value*100
+        inp2.textContent = result1
+    } 
+    if (inp2.value > 0){
+        const result1 = inp2.value*100
+        inp1.textContent = result1
+    }  
+}
+ 
