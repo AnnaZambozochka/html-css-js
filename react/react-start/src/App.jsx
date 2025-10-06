bc520469f949ba1eb3b2cd5e868ff0885b6cb7e0
AnnaZambozochka <80975250+AnnaZambozochka@users.noreply.github.com>
2025-10-06 17:41:30 +0300

    Училась

---
+++ react/react-start/src/App.jsx
@@ -0,0 +1,29 @@
+import Header from "./components/Header";
+import TeachingSection from "./components/TeachingSection";
+import DifferencesSection from "./components/DifferencesSection";
+import IntroSection from "./components/IntroSection";
+import TabsSection from "./components/collapse/TabsSection";
+import FeedbackSection from "./components/FeedbackSection";
+import React, { useState } from "react";
+import EffectSection from "./components/EffectSection";
+
+export default function App() {
+  const [tab, setTab] = useState("feedback");
+  return (
+    <>
+      <Header />
+      <main>
+        <IntroSection />
+        <TabsSection active={tab} onChange={(current) => setTab(current)} />
+        {tab === "main" && (
+          <>
+            <TeachingSection />
+            <DifferencesSection />
+          </>
+        )}
+        {tab === "feedback" && <FeedbackSection />}
+        {tab === "effect" && <EffectSection />}
+      </main>
+    </>
+  );
+}
